1621145711b49ed31a1bd73a18bf70c40b00da04
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-08-21 23:15:29 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
+      <Button>Press Me</Button>
       <StatusBar style="auto" />
     </View>
   );
