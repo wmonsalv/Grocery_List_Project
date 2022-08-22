53145ff6430c3c204b3f30b685f848dc2cc791f1
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-08-22 11:11:44 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Button title="Tap me"/>
+      <View style = {styles.inputContainer}>
+        <TextInput style = {styles.textInput} placeholder = "Add Item to the list"/>
+        <Button title="Add Item"/>
+      </View>
+      <View>
+        <Text>Grocery List</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+  },
+  inputContainer:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput:{
+    borderWidth : 1,
+    borderColor: '#cccccc',
+    width : "80%",
   },
 });
