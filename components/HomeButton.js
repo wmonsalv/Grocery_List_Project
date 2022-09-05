b718a45eee456c632eb9fef66bfec5bd6f108c2b
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-09-05 18:22:50 -0400

    Update HomeButton.js

--- components/HomeButton.js
+++ components/HomeButton.js
@@ -4,7 +4,7 @@ import { Button } from 'react-native-paper';
 //Note to self: PaperComponent is the component used for the add/cancel buttons on the ItemInput component
 
 const HomeButton = (props) => (
-  <Button onPress={props.click} textColor="black" buttonColor="white" mode="contained" style={{ width: 200, marginBottom: 80 }}>Welcome</Button>
+  <Button onPress={props.click} textColor="black" buttonColor="white" mode="contained" style={{ width: 200, marginBottom: 80 }}>Create a List</Button>
 );
 
 export default HomeButton;
