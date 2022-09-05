73902dd79143d85f380c68bb3d6622a401fa551c
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-09-05 00:47:47 -0400

    Update PaperComponent.js

--- components/PaperComponent.js
+++ components/PaperComponent.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Button } from 'react-native-paper';
 
+//Note to self: PaperComponent is the component used for the add/cancel buttons on the ItemInput component
+
 const PaperComponent = (props) => (
   <Button buttonColor="#1e90ff" mode="contained" onPress={props.onPress}>{props.title}</Button>
 );
