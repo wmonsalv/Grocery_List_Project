cc38735f9ecb774086845fe9a8255d2e491d8c35
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-09-05 12:00:50 -0400

    added logo

---
+++ components/Home.js
@@ -0,0 +1,11 @@
+import { Image, View } from "react-native"
+
+
+function Home() {
+
+    <Image source={require("/Users/william_x1/Documents/GitHub/expenses-app-main/Grocery_List_Project/assets/images/Shopping List.png")}/>
+
+
+}
+
+export default Home
--- components/Main.js
+++ components/Main.js
@@ -1,11 +1,20 @@
 import Main from "/Users/william_x1/Documents/GitHub/Grocery_List_Project-test_branch/components/Main.js";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
 
-  return (
-      <Main/>
-   )
-  
+    const Stack = createNativeStackNavigator();
 
-  }
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Main} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+
+
+
+}
 
