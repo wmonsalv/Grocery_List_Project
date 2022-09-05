0f05190867bce97b2edaffcb111be836a5fb6b40
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-09-05 11:36:40 -0400

    Main file created
    Will come in handy for navigational page

---
+++ components/Main.js
@@ -0,0 +1,11 @@
+import Main from "/Users/william_x1/Documents/GitHub/Grocery_List_Project-test_branch/components/Main.js";
+
+export default function App() {
+
+  return (
+      <Main/>
+   )
+  
+
+  }
+
