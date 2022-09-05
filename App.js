996aa31537d9e3341523dc48c645b0afe73e643f
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-09-05 13:36:55 -0400

    Created new component Main
    New component "Main" stores the other two components that allow the app to receive items from the user.

--- App.js
+++ App.js
@@ -1,69 +1,10 @@
-import { useState } from 'react';
-import { StyleSheet, View, Button, TextInput, FlatList } from 'react-native';
-import GroceryItem from './components/GroceryItem';
-import ItemInput from './components/ItemInput';
-
+import Main from "/Users/william_x1/Documents/GitHub/expenses-app-main/Grocery_List_Project/components/Main.js";
 
 export default function App() {
 
-  const [listOfItems, setListofItems] = useState([])
-
-  const [modalIsVisible, setModalisVisible] = useState(false)
-
-  function startAddItemHandler(){
-    setModalisVisible(true)
-  }
-
-  function endAddItemHandler(){
-    setModalisVisible(false)
-  }
-
-  const [placeholder, setPlaceHolder] = useState("Add Item to the list")
-
-  //This placeholder state editor above is tho display a different message if an empty string is submitted by the user
-
-  function addItemHandler(enteredItemText) {
-   
-    if(enteredItemText !== "" || listOfItems.find((item) => {item.text !== enteredItemText})){
-      setListofItems((currentListOfItems) => [...currentListOfItems, { text: enteredItemText, key: Math.random().toString() }]) //This is updating the old grocery list state based on the old grocery list state by appending a new enteredItemText
-      setModalisVisible(false)
-      setPlaceHolder("Add Item to the list")
-    } 
-    else{
-      setPlaceHolder("Please type the name of an item")
-    }
-  }
-
-  //every item in my array is now an object with these attributes, entered text and a key
-  //At the bottom, we have to do itemData.item.text to access the data attribute of the object
-
-  function deleteItemHandler(text){
-    setListofItems(currentListOfItems => { return currentListOfItems.filter((item) => item.text !== text)})
-  }
-
-  //Here, I'm using the onDelete prop to pass down the deleteHandler function to the GroceryItem component so that items are deleted when clicked
-
   return (
-    <View style={styles.container}>
-      <Button title="Add new Item" color="#1e90ff" onPress={startAddItemHandler}/>
-      <ItemInput placeHolderChanger={placeholder} visible={modalIsVisible} onAddInput={addItemHandler} onCancel={endAddItemHandler}/>
-      <View style={styles.listContainer}>
-        <FlatList data={listOfItems} alwaysBounceVertical={false} renderItem={(itemData) => {
-          return (<GroceryItem text={itemData.item.text} id={itemData.item.text} onDelete={deleteItemHandler}/>)
-        }}>
-        </FlatList>
-      </View>
-    </View>
-  );
-}
+      <Main/>
+   )
+  
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16
-  },
-  listContainer: {
-    flex: 5,
   }
-});
--- components/Main.js
+++ components/Main.js
@@ -1,20 +1,106 @@
-import Main from "/Users/william_x1/Documents/GitHub/Grocery_List_Project-test_branch/components/Main.js";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useState } from 'react';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
+import GroceryItem from "/Users/william_x1/Documents/GitHub/expenses-app-main/Grocery_List_Project/components/GroceryItem.js"
+import ItemInput from "/Users/william_x1/Documents/GitHub/expenses-app-main/Grocery_List_Project/components/ItemInput.js"
 
-export default function App() {
+function Main() {
 
-    const Stack = createNativeStackNavigator();
+const [listOfItems, setListofItems] = useState([])
 
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={Main} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+const [modalIsVisible, setModalisVisible] = useState(false)
 
+function startAddItemHandler(){
+  setModalisVisible(true)
+}
 
+function endAddItemHandler(){
+  setModalisVisible(false)
+}
+
+const [placeholder, setPlaceHolder] = useState("Add Item to the list")
+
+//This placeholder state editor above is tho display a different message if an empty string is submitted by the user
+
+function addItemHandler(enteredItemText) {
+ 
+  if(enteredItemText !== "" || listOfItems.find((item) => {item.text !== enteredItemText})){
+    setListofItems((currentListOfItems) => [...currentListOfItems, { text: enteredItemText, key: Math.random().toString() }]) //This is updating the old grocery list state based on the old grocery list state by appending a new enteredItemText
+    setModalisVisible(false)
+    setPlaceHolder("Add Item to the list")
+  } 
+  else{
+    setPlaceHolder("Please type the name of an item")
+  }
+}
+
+//every item in my array is now an object with these attributes, entered text and a key
+//At the bottom, we have to do itemData.item.text to access the data attribute of the object
+
+function deleteItemHandler(text){
+  setListofItems(currentListOfItems => { return currentListOfItems.filter((item) => item.text !== text)})
+}
+
+//Here, I'm using the onDelete prop to pass down the deleteHandler function to the GroceryItem component so that items are deleted when clicked
+
+return (
+    <View style={styles.container}>
+    <Button title="Add new Item" color="#1e90ff" onPress={startAddItemHandler}/>
+    <ItemInput placeHolderChanger={placeholder} visible={modalIsVisible} onAddInput={addItemHandler} onCancel={endAddItemHandler}/>
+    <View style={styles.listContainer}>
+      <FlatList data={listOfItems} alwaysBounceVertical={false} renderItem={(itemData) => {
+        return (<GroceryItem text={itemData.item.text} id={itemData.item.text} onDelete={deleteItemHandler}/>)
+      }}>
+      </FlatList>
+    </View>
+  </View>
+)
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 50,
+      paddingHorizontal: 16
+    },
+    listContainer: {
+      flex: 5,
+    }
+  });
+
+export default Main;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Main from "/Users/william_x1/Documents/GitHub/Grocery_List_Project-test_branch/components/Main.js";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// export default function App() {
+
+//     const Stack = createNativeStackNavigator();
+
+//     return (
+//         <NavigationContainer>
+//             <Stack.Navigator>
+//                 <Stack.Screen name="Home" component={Main} />
+//             </Stack.Navigator>
+//         </NavigationContainer>
+//     );
+
+
+
+// }
 
