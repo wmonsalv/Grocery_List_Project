edbc1e13590f0b5fc85486d97f2aab5aae9f7020
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-10-06 10:01:48 -0400

    improved home page UI

--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,4 @@
-import { Image, View, Text, StyleSheet } from "react-native"
+import { Image, View, Text, StyleSheet, TouchableOpacity } from "react-native"
 import HomeButton from "./HomeButton"
 
 //this is just a test to make sure that I'm on the firebase branch 
@@ -10,9 +10,13 @@ function Home({navigation}) {
 
         <View style={styles.Background}>
         <Image style={styles.imageEdit} source={require("/Users/william_x1/Documents/GitHub/expenses-app-main/Grocery_List_Project/assets/images/ShoppingList.png")}/>
-        <View style={styles.justifyContents} >
-        <HomeButton buttonName="Create a List" click={() => navigation.navigate("Make a List")}/>
-        <HomeButton buttonName="Login" click={() => navigation.navigate("Login")}/>
+        <View style={styles.buttonContainer} >
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Make a List")}>
+        <Text style={styles.buttonText}>Make a List</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Login")}>
+        <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
         </View>
         </View>
     )
@@ -37,7 +41,27 @@ const styles = StyleSheet.create({
         height: "100%",
         widht: "100"
 
-    }
+    },
+    button: {
+        backgroundColor: "white",
+        width: "100%",
+        padding: 15,
+        borderRadius: 10,
+        alignItems: "center",
+        marginBottom: 10
+    },
+    buttonText: {
+        color: "#27963C",
+        fontWeight: "700",
+        fontSize: 16
+    },
+    buttonContainer: {
+        width: "60%",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+        marginTop: 425
+    },
     })
 
 export default Home
