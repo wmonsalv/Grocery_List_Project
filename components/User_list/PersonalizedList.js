f7c9eaefca7cdcbd068f5fe94c206ec66860ac4a
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-10-11 00:28:22 -0400

    updated styling on component
    Updated style on user lists component

--- components/User_list/PersonalizedList.js
+++ components/User_list/PersonalizedList.js
@@ -1,5 +1,6 @@
 import { useNavigation } from "@react-navigation/native"
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
 import { auth } from "../../firebase"
 
 const PersonalizedList = () => {
@@ -19,12 +20,17 @@ const PersonalizedList = () => {
     //We use a question mark because that tells javascript that this "User.email" might be undefined (which if we leave as is and it is undefined, it will crash our app). Hence, this tells js, if it's undefined, don't check for the email.
 
 return(
-    <View style={styles.container}>
-       <Text>Email: {auth.currentUser?.email}</Text>  
+    
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#ededed" }}>
+    <View style={styles.header}>
+    <Text style={{ fontWeight: "bold", fontSize: 15, color: "black" }}>Email: {auth.currentUser?.email}</Text>  
+    </View>
+    <View style={styles.footer}>
        <TouchableOpacity onPress={handleSignOut} style={styles.button}>
         <Text style={styles.buttonText}>Sign out</Text>
        </TouchableOpacity>
     </View>
+    </SafeAreaView>
 )
 
 }
@@ -32,10 +38,18 @@ return(
 export default PersonalizedList
 
 const styles = StyleSheet.create({
+    header: {
+        backgroundColor:"#E0E0E0",
+        padding: 20,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        bottom: 60
+      },
     container: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
     },
     button: {
         backgroundColor: "#27963C",
@@ -50,4 +64,14 @@ const styles = StyleSheet.create({
         fontWeight: "700",
         fontSize: 16
     },
+    footer: {
+        position: "absolute",
+        bottom: 25,
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingHorizontal: 20,
+        color: "#fff"
+      },
 })
